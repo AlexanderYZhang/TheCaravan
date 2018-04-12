e9a57c5a0003fa11ccd0b23f1e4facf7ec9337b9
Alex <ayzhang@umd.edu>
2018-04-12 19:20:34 -0400

    Rock resource generation, scaled trees and terrain by 3, trees now rest on top of protrusions rather than inside

--- Assets/Scripts/TerrainGenerator.cs
+++ Assets/Scripts/TerrainGenerator.cs
@@ -6,7 +6,7 @@ public class TerrainGenerator : MonoBehaviour {
 	public GameObject player;
 	public GameObject[] Tile;
 	public GameObject[] Trees;
-	public GameObject[] protrusions;
+	public GameObject[] Protrusions;
 	public GameObject[] Rocks;
 	
 	public int width;
@@ -17,10 +17,11 @@ public class TerrainGenerator : MonoBehaviour {
 	private Transform tileHolder;
 	private Transform treeHolder;
 	private Transform protrusionHolder;
+	private Transform resourceHolder;
 	private Vector3 position;
 	private Vector3 scale;
 	private int[,] board;
-	private int[,] treeBoard;
+	private int[,] protrusionBoard;
 
 	private enum TerrainItem {
 		Nothing = 0,
@@ -32,111 +33,150 @@ public class TerrainGenerator : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
-		int heightCoord = height * 3;
-		int widthCoord = width * 3;
-
+		int heightCoord = height;
+		int widthCoord = width;
+		offsetX = Random.Range(0, 9999f);
+		offsetY = Random.Range(0, 9999f);
+		
 		Random.InitState((int)System.DateTime.Now.Ticks);
-		board = new int[heightCoord, widthCoord];
-		treeBoard = new int[heightCoord, widthCoord];
-		InitBoard(board);
-		InitBoard(treeBoard);
+		InitBoard();
 
 		tileHolder = new GameObject("Board").transform;
 		treeHolder = new GameObject("Trees").transform;
 		protrusionHolder = new GameObject("Protrusions").transform;
+		resourceHolder = new GameObject("Resources").transform;
 
 		position = new Vector3(0, 0, 0);
 		scale = new Vector3(0, 0, 0);
-		offsetX = Random.Range(0, 9999f);
-		offsetY = Random.Range(0, 9999f);
+
 		
-		int[] distribution = {0,0,0,0,0,0,0,0,0,0,0};
-		for (int r = 0; r < widthCoord; r++) {
-			for (int c = 0; c < heightCoord; c++) {
-				int tileIndex = randomTile(r, c);
-				distribution[tileIndex]++;
-
-				if (r % 3 == 0 && c % 3 == 0) {
-					position.Set(r, 0, c);
-					GameObject tileToInstantiate = Instantiate(Tile[tileIndex%2], position, Quaternion.identity);
-					tileToInstantiate.transform.SetParent(tileHolder.gameObject.transform);
+		for (int r = 0; r < width; r++) {
+			for (int c = 0; c < height; c++) {
+				float offset = 0f;
+				if (protrusionBoard[r,c] != (int) TerrainItem.Nothing) {
+					offset = protrusionPlacement(r, c);
 				}
 
-				if (tileIndex <= 2 || tileIndex >= 8) {
-					treePlacement(r, c);
+				if (board[r,c] == (int) TerrainItem.Tree) {
+					treePlacement(3 * r, 3 * c, offset);
 				}
 
-				if (tileIndex >= 7) {
-					protrusionPlacement(r, c, tileIndex);
+				if (board[r,c] == (int) TerrainItem.Resource) {
+					rockPlacement(3 * r, 3 * c);
 				}
+
+				// position.Set(3 * r, 0, 3 * c);
+				// GameObject tileToInstantiate = Instantiate(Tile[randomTile(r,c)%2], position, Quaternion.identity);
+				// tileToInstantiate.transform.SetParent(tileHolder.gameObject.transform);
 			}
 		}
 
 		player.transform.position = new Vector3(width*3/2, 1.5f, height*3/2);
-		for (int i = 0; i < distribution.Length; i++) {
+	}
+	
+	private void InitBoard() {
+		int numRocks = 50, i = 0;
+		board = new int[height, width];
+		protrusionBoard = new int[height, width];
+
+		int[] distribution = {0,0,0,0,0,0,0,0,0,0,0};
+		for (int x = 0; x < height; x++) {
+			for (int y = 0; y < width; y++) {
+				int tileIndex = randomTile(x,y);
+				float randomTree = Random.Range(0, 2f);
+				
+				distribution[tileIndex]++;
+				
+				board[x,y] = (int)TerrainItem.Nothing;
+				protrusionBoard[x,y] = (int)TerrainItem.Nothing;
+				if ((tileIndex <= 2 || tileIndex >= 8) && randomTree > 1.5f) {
+					board[x,y] = (int) TerrainItem.Tree;
+				}	
+
+				if (tileIndex >= 7) {
+					protrusionBoard[x,y] = (int) TerrainItem.Protrusion;
+				}
+			}
+		}
+
+		while(i < numRocks) {
+			i++;
+			int randX = (int) Random.Range(0, height);
+			int randY = (int) Random.Range(0, width);
+
+			if (protrusionBoard[randX, randY] == (int)TerrainItem.Nothing &&
+				board[randX, randY] == (int)TerrainItem.Nothing) {
+					board[randX, randY] = (int)TerrainItem.Resource;
+			}
+		}
+
+		for (i = 0; i < distribution.Length; i++) {
 			print(distribution[i]);
 		}
 	}
-	
-	private void InitBoard(int[,] board) {
-		for (int x = 0; x < height * 3; x++) {
-			for (int y = 0; y < width * 3; y++) {
-				board[x,y] = (int)TerrainItem.Nothing;
-			}
-		}
-	}
-	private void protrusionPlacement(int r, int c, int tileIndex) {
+
+	private void rockPlacement(int r, int c) {
+		int randRock = (int) Random.Range(0, 1f);
 		position.Set(r, 0, c);
+		GameObject rockToInstantiate = Instantiate(Rocks[0], position, Quaternion.identity);
+		rockToInstantiate.transform.SetParent(resourceHolder.gameObject.transform);
+	}
+
+	private float protrusionPlacement(int r, int c) {
+		int tileIndex = randomTile(r, c);
+		float offset = 0f;
+		position.Set(r*3, 0, c*3);
 		GameObject protrusionToInstantiate;
 		switch (tileIndex) {
 			case 7: 
-				protrusionToInstantiate = Instantiate(protrusions[0], position, Quaternion.identity);
+				protrusionToInstantiate = Instantiate(Protrusions[0], position, Quaternion.identity);
+				offset = 2.5f;
 				break;
 			case 8: 
-				protrusionToInstantiate = Instantiate(protrusions[1], position, Quaternion.identity);
+				protrusionToInstantiate = Instantiate(Protrusions[1], position, Quaternion.identity);
+				offset = 2.7f;
 				break;
 			case 9:
-				protrusionToInstantiate = Instantiate(protrusions[2], position, Quaternion.identity);
+				protrusionToInstantiate = Instantiate(Protrusions[2], position, Quaternion.identity);
+				offset = 4.8f;
 				break;
 			case 10:
-				protrusionToInstantiate = Instantiate(protrusions[2], position, Quaternion.identity);
+				protrusionToInstantiate = Instantiate(Protrusions[2], position, Quaternion.identity);
+				offset = 4.8f;
 				break;
 			default:
 				protrusionToInstantiate = new GameObject("empty");
 				break;
 		}
 		protrusionToInstantiate.transform.SetParent(protrusionHolder.gameObject.transform);
+		return offset;
 	}
 
-	private void treePlacement(int r, int c) {
+	private void treePlacement(int r, int c, float offset) {
 		float scaleFactor = 0f;
 		float randomTree = Random.Range(0f, 2f);
 		GameObject treeToInstantiate;
 
-		if (randomTree < 1.9f) {
-			return;
-		}
-
 		randomTree = Random.Range(0,3);
 
 		if (randomTree < 1) {
-			scaleFactor = 1 + Random.Range(0, .75f);
+			scaleFactor =  3 + Random.Range(0, .75f);
 
-			position.Set(r, .75f, c);
+			position.Set(r, .75f + offset, c);
 			scale.Set(scaleFactor, scaleFactor, scaleFactor); 
 
 			treeToInstantiate = Instantiate(Trees[0], position, Quaternion.identity);
 		} else if (randomTree >= 1 && randomTree < 2) {
-			scaleFactor = 1 + Random.Range(0, .75f);
+			scaleFactor = 3 + Random.Range(0, .75f);
 
-			position.Set(r, .3f, c);
+			position.Set(r, .3f + offset, c);
 			scale.Set(scaleFactor, scaleFactor, scaleFactor); 
 
 			treeToInstantiate = Instantiate(Trees[1], position, Quaternion.identity);
 		} else {
-			scaleFactor = 1 + Random.Range(0, .75f);
+			scaleFactor = 3 + Random.Range(0, .75f);
 
-			position.Set(r, .3f, c);
+			position.Set(r, .3f + offset, c);
 			scale.Set(scaleFactor, scaleFactor, scaleFactor); 
 
 			treeToInstantiate = Instantiate(Trees[2], position, Quaternion.identity);
