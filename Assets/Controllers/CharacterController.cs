bbee11448f43451e540661c585c8703398128c89
j-pai <jesse.pai@gmail.com>
2018-04-11 21:19:21 -0400

    Added animations to CharacterObject
    Added initial script file for player object.
    Added fields/initialization code for camera positioning.
    Redid several animations to improve looping/trainsitions.
    Added animation controller with state machine for better animation transitions.

---
+++ Assets/Controllers/CharacterController.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CharacterController : MonoBehaviour {
+    public GameObject mainCam;
+    public Vector3 mainCamOffset;
+    public Vector3 mainCamRot;
+
+    private Animator anim;
+
+    // Use this for initialization
+    void Start () {
+        anim = GetComponent<Animator>();
+        mainCam.transform.position = mainCamOffset;
+        mainCam.transform.eulerAngles = mainCamRot;
+        anim.SetTrigger("Idle");
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        
+	}
+}
