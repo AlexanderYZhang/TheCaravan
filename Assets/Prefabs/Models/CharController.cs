b47c412ac6c4bd1e4746589a57d463939b2d7be9
j-pai <jesse.pai@gmail.com>
2018-04-29 23:15:23 -0400

    Updates to character script to include message and item panel.

--- Assets/Prefabs/Models/CharController.cs
+++ Assets/Prefabs/Models/CharController.cs
@@ -1,44 +1,63 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.AI;
+using UnityEngine.UI;
 
 public class CharController : MonoBehaviour {
-    // Camera Setup
-    public GameObject mainCam;
-    private Vector3 mainCamOffset = new Vector3(0, 20, -20);
-    private Vector3 mainCamRot = new Vector3(45, 0, 0);
+    public Camera cam;
+    public GameObject car;
+    public GameObject canvas;
+    public NavMeshAgent agent;
 
-    // Character Parameters
-    private float walkSpeed = 5;
-    private float runSpeed = 15;
-
+    // Character Parameter
     private Animator anim;
+    private bool placed;
+    private Text[] texts;
+    private GameObject message;
+    private GameObject itemBar;
+
+    public bool IsPlaced() {
+        return placed;
+    }
+
+    public void SetPlaced() {
+        if (!placed) {
+            placed = true;
+        }
+    }
 
     // Use this for initialization
     void Start () {
         anim = GetComponent<Animator>();
-        mainCam.transform.position = transform.position + mainCamOffset;
-        mainCam.transform.eulerAngles = mainCamRot;
         anim.SetTrigger("moving");
+        placed = false;
+        texts = canvas.GetComponentsInChildren<Text>();
+        message = GameObject.Find("MessagePanel");
+        itemBar = GameObject.Find("ItemBarPanel");
+        message.SetActive(false);
+        texts[0].text = "BLANK";
     }
-	
+
 	// Update is called once per frame
 	void Update () {
-        Vector2 input = new Vector3(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
-        Vector2 inputDir = input.normalized;
+        if (!agent.isActiveAndEnabled && GetComponent<Rigidbody>().velocity.y == 0) {
+            agent.enabled = true;
+        } else if (agent.isActiveAndEnabled) {
+            bool running = Input.GetKey(KeyCode.LeftShift);
 
-        if (inputDir != Vector2.zero) {
-            transform.eulerAngles = Vector3.up * Mathf.Atan2(inputDir.x, inputDir.y) * Mathf.Rad2Deg;
+            if (Input.GetMouseButton(0)) {
+                Ray ray = cam.ScreenPointToRay(Input.mousePosition);
+                RaycastHit hit;
+
+                if (Physics.Raycast(ray, out hit)) {
+                    agent.SetDestination(hit.point);
+                }
+            }
+
+            agent.speed = running ? 15f : 5f;
+            float animSpeedPct = (running ? 1.0f : 0.5f) * (agent.hasPath ? 1 : 0);
+            anim.SetFloat("speedPct", animSpeedPct);
         }
-
-        bool running = Input.GetKey(KeyCode.LeftShift);
-        float moveS = (running ? runSpeed : walkSpeed) * inputDir.magnitude;
-
-        float animSpeedPct = (running ? 1.0f : 0.5f) * inputDir.magnitude;
-
-        transform.Translate(transform.forward * moveS * Time.deltaTime, Space.World);
-        anim.SetFloat("speedPct", animSpeedPct);
-
-        mainCam.transform.position = transform.position + mainCamOffset;
     }
 }
